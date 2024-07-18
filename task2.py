2c4e57b8dae80a9f87166e0413426928976b24e9
G0rde3V <gordeev-5@yandex.ru>
2024-07-18 10:49:10 +0300

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,10 @@
+# "1st program"
+print((9 ** 0.5) * 5)
+# "2st program"
+print(99.9 > 9.98 and 1000 != 1000.1)
+# "3st program"
+print(((1234 % 1000) // 10) + (5678 % 1000) // 10)
+# "4st program"
+a = 13.42
+b = 42.13
+print((int(a)) == (int(b % 42 * 100)) or (int(b)) == (int(a % 13 * 100)))
